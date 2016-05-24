d6e16f4449a0e1f10fc939ca2138560dfb8d1a65
XelaLord <alex.m.boyer@gmail.com>
2016-05-24 06:37:31 -0400

    Rename DriverFunction.ino to RobotCode/DriverFunction.ino

---
+++ RobotCode/DriverFunction.ino
@@ -0,0 +1,32 @@
+float leftSidePower;
+float rightSidePower;
+
+void DriveFunction(int x, int y) {
+    leftSidePower = (-x*255)+(y*255);
+    rightSidePower = (-x*255)+(-y*255);
+    
+    if(leftSidePower >255){
+      leftSidePower = 255;
+     }
+    else if (leftSidePower < -255){
+      leftSidePower = -255;
+     }
+    if(rightSidePower >255){
+      rightSidePower = 255;
+     }
+    else if (rightSidePower <-255){
+      rightSidePower = -255;
+     }
+    if(leftSidePower < 0){
+        DriveMotorL(leftSidePower,true);
+      }
+    if (leftSidePower>0){ 
+        DriveMotorL(leftSidePower,false);
+      } 
+    if(rightSidePower < 0){
+        DriveMotorR(rightSidePower,true);
+      }
+    if (rightSidePower>0){
+        DriveMotorR(rightSidePower,false);
+      } 
+  }
